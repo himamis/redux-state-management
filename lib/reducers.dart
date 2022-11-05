b21703020d3a1a0f98ed5d3fd17761075d8a7b6d
Balazs Bencze <balazs.bencze@geogebra.org>
2022-11-05 14:00:23 +0200

    Combine reducers

--- lib/reducers.dart
+++ lib/reducers.dart
@@ -1,17 +1,20 @@
+import 'package:redux/redux.dart';
 import 'package:redux_state_management/actions.dart';
 import 'package:redux_state_management/state.dart';
 
-MoviesAppState moviesAppStateReducer(MoviesAppState state, dynamic action) {
-  if (action is StartLoading) {
-    // Notice how we can simply rebuild the object without needing to
-    // specify the arguments that do not change
-    return state.rebuild((b) => b..isLoading = true);
-  } else if (action is MoviesLoaded) {
-    return state.rebuild(
-      (b) => b
-        ..isLoading = false
-        ..movies.replace(action.moviesLoaded),
-    );
-  }
-  return state;
+MoviesAppState _startLoadingReducer(MoviesAppState state, StartLoading action) {
+  return state.rebuild((b) => b.isLoading = true);
 }
+
+MoviesAppState _moviesLoadedReducer(MoviesAppState state, MoviesLoaded action) {
+  return state.rebuild(
+    (b) => b
+      ..isLoading = false
+      ..movies.replace(action.moviesLoaded),
+  );
+}
+
+final moviesAppStateReducer = combineReducers<MoviesAppState>([
+  TypedReducer(_startLoadingReducer),
+  TypedReducer(_moviesLoadedReducer),
+]);
